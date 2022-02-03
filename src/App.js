6efe5ff77d70f907c56e44aca47542e1e86e41d3
aiden <kingoxpo@gamil.com>
2022-02-03 15:39:21 +0900

    add coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,51 +1,10 @@
 import { useEffect, useState } from "react/cjs/react.development";
 
-function BitToKRW() {
-  const [amount, setAmount] = useState();
-  const [inverted, setInverted] = useState(false);
-  const onChange = event => {
-    setAmount(event.target.value);
-  };
-  const resetAmount = () => setAmount("");
-  const onInvert = () => {
-    resetAmount();
-    setInverted(current => !current);
-  };
-  return (
-    <div>
-      <div>
-        <input
-          value={inverted ? (amount * 0.01).toFixed(2) : amount}
-          placeholder="비트코인(BIT)"
-          type="number"
-          onChange={onChange}
-          disabled={inverted}
-          id="BIT"
-        />
-        <label htmlFor="BIT">파운드(£)</label>
-      </div>
-      <div>
-        <input
-          value={inverted ? (amount * 0.2).toFixed(2) : amount}
-          placeholder="원(WON)"
-          type="number"
-          onChange={onChange}
-          disabled={!inverted}
-          id="KRW"
-        />
-        <label htmlFor="KRW">파운드(£)</label>
-      </div>
-      <button onClick={resetAmount}>새로고침</button>
-      <button onClick={onInvert}>
-        {inverted ? "단위전환 비트코인 > 원 " : "단위전환 원 > 비트코인"}
-      </button>
-    </div>
-  );
-}
-
 export default function App() {
   const [loding, setLoding] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [userMoney, setUserMoney] = useState([]);
+
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then(response => response.json())
@@ -54,27 +13,43 @@ export default function App() {
         setLoding(false);
       });
   }, []);
-  const [coinId, setCoinId] = useState("");
-  const onSelect = event => {
-    setCoinId(event.target.value);
-  };
 
+  // const inputPriceFormat = str => {
+  //   const comma = str => {
+  //     str = String(str);
+  //     return str.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  //   };
+  //   const uncomma = str => {
+  //     str = String(str);
+  //     return str.replace(/[^\d]+/g, "");
+  //   };
+  //   return comma(uncomma(str));
+  // };
   return (
     <div>
-      <h1>Welcome to coin of the world {loding ? "" : `(${coins.length})`}</h1>
+      <h1>Welcome to the coin world {loding ? "" : `(${coins.length})`}</h1>
+      <h2>How much do you have?</h2>
+      <input
+        // onChange={e => setUserMoney(inputPriceFormat(e.target.value))}
+        onChange={e => setUserMoney(e.target.value)}
+        type="text"
+        value={userMoney}
+        placeholder="Please write your KRW"
+      />
+      <label>원(₩)</label>
+      <hr />
       {loding ? (
         <strong>Loading...</strong>
       ) : (
-        <select value={coins.id}>
+        <select>
           {coins.map((coin, index) => (
             <option key={index}>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price.toFixed(2)}{" "}
-              USD
+              {coin.name} ({coin.symbol}):{" "}
+              {userMoney / 1205.6 / coin.quotes.USD.price} {coin.symbol}
             </option>
           ))}
         </select>
       )}
-      <hr />
     </div>
   );
 }
