4459db150e8b93285b1b99c74007873700e5ee12
aiden <kingoxpo@gamil.com>
2022-02-08 14:33:42 +0900

    m

--- src/App.js
+++ src/App.js
@@ -5,15 +5,10 @@ import Home from "./routes/Home";
 export default function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <div>
-        <Routes>
-          <Route
-            path={`${process.env.PUBLIC_URL}/movie/:id`}
-            element={<Detail />}
-          />
-          <Route path={`${process.env.PUBLIC_URL}`} element={<Home />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/movie/:id" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
